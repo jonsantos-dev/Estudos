1440d3f30a159948d04403ecbd5cea514e6dce5d
Mahogany741 <jonathan741.js@gmail.com>
2025-12-27 23:05:03 -0300

    Anotacoes da aula de metodos estaticos

---
+++ metodos-estaticos.js
@@ -0,0 +1,28 @@
+//Métodos estáticos
+class ControleRemoto {
+    constructor(tv) {
+        this.tv = tv;
+        this.volume = 0;
+    }
+
+    // Método de instância
+    aumentarVolume() {
+        this.volume += 2;
+    }
+
+    // Método de instância
+    diminuirVolume() {
+        this.volume -= 2;
+    }
+
+    //Método estático
+    static trocaPilha() {
+        console.log("Ok vou trocar!")
+    }
+}
+
+const controle1 = new ControleRemoto('LG');
+// const controle2 = new ControleRemoto('Samsung');
+controle1.aumentarVolume();
+console.log(controle1);
+ControleRemoto.trocaPilha();
